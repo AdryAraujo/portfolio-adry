69d2987664a72d90414ce3dcc927c06abf0e3c6b
Adryelle Silva <191036532940@academico.ufs.br>
2024-06-13 10:41:42 -0300

    criando a secção de descrição e resposividade

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,10 @@ import bolha1 from './assets/bolha1.svg'
 import bolha2 from './assets/bolha2.svg'
 import bolha3 from './assets/bolha3.svg'
 import bolha4 from './assets/bolha4.svg'
-import perfil from './assets/perfil.png';
+// import perfil from './assets/perfil.png';
 import computer from './assets/computer.svg';
 import logo from './assets/logo.svg';
+import perfil from './assets/adry.svg';
 
 // icones
 import { MdHome } from "react-icons/md";
@@ -38,47 +39,49 @@ function App() {
 
       {/* Barra de navegação */}
       <nav>
-      <img src={logo} alt="" />
-      <button
-				className="menu"
-        onClick={() => setMenuOpen(!menuOpen)}>
-				<FaBars className='menu-icon'/>
-			</button>
-      <ul className={menuOpen ? "open" : ""}>
-        <li>
-        <a href="/">Home</a>
-        </li>
-        <li>
-        <a href="/">Sobre Mim</a>
-        </li>
-        <li>
-        <a href="/">Ferramentas</a>
-        </li>
-        <li>
-        <a href="/">Contato</a>
-        </li>
-      </ul>
-    </nav>
-    
-      {/* <img id='perfil' src={perfil} alt="logo" /> */}
-      <div className="container">
-        {/* <img className='bolha-left' src={bolha1} alt="logo" /> */}
-        {/* <div className="descricao">
-          <h1>Adryelle Araujo da Silva</h1>
+        <img src={logo} alt="" />
+        <button
+          className="menu"
+          onClick={() => setMenuOpen(!menuOpen)}>
+          <FaBars className='menu-icon' />
+        </button>
+        <ul className={menuOpen ? "open" : ""}>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/">Sobre Mim</a>
+          </li>
+          <li>
+            <a href="/">Ferramentas</a>
+          </li>
+          <li>
+            <a href="/">Contato</a>
+          </li>
+        </ul>
+      </nav>
+
+      {/* descrição */}
+      <div className="container-descricao">
+        <img className='perfil' src={perfil} alt="logo" />
+        <div className="descricao">
+          <h1>Adryelle Araujo</h1>
           <h3>Front-end Developer | WebDsegin | Power Bi</h3>
           <p>Estudante de Ciência da Computação pela Universidade Federal de Sergipe, desenvolvedora Web com foco em Frontend, aprendendo cada vez mais Web Design. Apaixonada em descobrir coisas novas.</p>
-        </div> */}
-        {/* <img className='bolha-right' src={bolha2} alt="logo" /> */}
+        </div>
       </div>
-      {/*<div className='sobreMim'>
-      <div className='sobreMimText'>
-        <h1>Sobre Mim</h1>
-        <p>Atuei 2 anos na Softeam, empresa Junior da Universidade Federal de Sergipe, como desenvolvedora de sites com WordPress. Bolsista do Instituto Federal de Sergipe, desenvolvendo dashboards em Power Bi e dando manutenção no site com o cms Joomla. Além de ser estagiária na Secretaria de Estado da Educação e da Cultura de Sergipe, na qual ajudo nos dashboards em Power Bi.</p>
-        <button className='button'>Saiba Mais</button>
-      </div>
-      <img src={computer} alt='computer' />
-    </div>
-    <div className='tools'>
+
+      {/* Sobre mim */}
+      {/* <div className='sobreMim'>
+        <div className='sobreMimText'>
+          <h1>Sobre Mim</h1>
+          <p>Atuei 2 anos na Softeam, empresa Junior da Universidade Federal de Sergipe, como desenvolvedora de sites com WordPress. Bolsista do Instituto Federal de Sergipe, desenvolvendo dashboards em Power Bi e dando manutenção no site com o cms Joomla. Além de ser estagiária na Secretaria de Estado da Educação e da Cultura de Sergipe, na qual ajudo nos dashboards em Power Bi.</p>
+          <button className='button'>Saiba Mais</button>
+        </div>
+        <img src={computer} alt='computer' />
+      </div> */}
+
+      {/*<div className='tools'>
     <img className='bolha-left' src={bolha3} alt="logo" />
     <img className='bolha-right' src={bolha4} alt="logo" />
       <div className='tools-card'>
